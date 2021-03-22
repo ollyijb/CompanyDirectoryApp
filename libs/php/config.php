40fe0ea1931f2ff844f94b18c6fac05ff0ab7208
ollyijb <ollyijb@hotmail.com>
2021-03-22 13:55:22 +0000

    minor styling of alert modals

--- libs/php/config.php
+++ libs/php/config.php
@@ -1,21 +1,21 @@
 <?php
-
+/* 
 	$cd_host = "db5001989549.hosting-data.io";
 	$cd_port = 3306;
 	$cd_socket = "";
 	$cd_user = "dbu271528"; // user name
 	$cd_password = "Danter548834!"; // password
-	$cd_dbname = "dbs1624047"; // database name
+	$cd_dbname = "dbs1624047"; // database name */
 
 	
-/* 	$cd_host = "127.0.0.1";
+	$cd_host = "127.0.0.1";
 	$cd_port = 3306;
 	$cd_socket = "";
 	$cd_user = "user"; // user name
 	$cd_user = 'root';
 	$cd_password = "sElling@348"; // password
 	$cd_password = 'NewPassword';
-	$cd_dbname = "companydirectory"; // database name */
+	$cd_dbname = "companydirectory"; // database name
 	
 
 ?>
